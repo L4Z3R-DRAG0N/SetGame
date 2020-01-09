e761e8d626769a5ec7b33856fa3db744ffaf090c
Lazer_Potato <roogie0610@outlook.com>
2020-01-09 13:24:33 -0800

    crappy solver logic

---
+++ project_set_solver.py
@@ -0,0 +1,14 @@
+def set_solver(cards):
+    set = [[] for j in range(4)]
+    for i in range(12):
+        attributeCount = 0
+        for j in range(-11):
+            if cards[i][0]==cards[j][0]
+                
+                
+                
+                
+                attributeCount += 1
+        
+        if attributeCount == 4:
+            set[].append()
